44b31f75779ee971de5c6305afe300121c02135e
GMorgado <gnf.morgado@gmail.com>
2025-03-26 11:52:15 +0000

    feat: categorias (controller, identity, repository), sem acesso

---
+++ src/main/java/upskill/daterra/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package upskill.daterra.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import upskill.daterra.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByName(String name);
+}
